a47b2cf0ce2f1e7eb93907925ba3b79c043b78d0
andromeda951 <andromeda951@gmail.com>
2026-08-27 21:04:25 +0700

    update

--- for_loop.py
+++ for_loop.py
@@ -1,6 +1,6 @@
-# for i in range(5):
-#     print("hello")
-#     print("test")
+for i in range(5):
+    print("hello")
+    print("test")
 
 # i=0   print hello print test
 # i=1   print hello print test
@@ -8,8 +8,8 @@
 # i=3   print hello print test
 # i=4   print hello print test
 
-# for i in range(5):
-#     print(i)
+for i in range(5):
+    print(i)
 # i=0   print 0
 # i=1   print 1
 # i=2   print 2
@@ -17,11 +17,11 @@
 # i=4   print 4
 
 # total=0+1+2+3+4=10
-# total = 0
-# for i in range(5):
-#     total = total + i
-# print(total)
-# print(total/5)
+total = 0
+for i in range(5):
+    total = total + i
+print(total)
+print(total/5)
 
 # i=0   total=0+0=0
 # i=1   total=0+1=1
@@ -29,11 +29,11 @@
 # i=3   total=3+3=6
 # i=4   total=6+4=10
 
-# total = 0
-# for i in range(5):
-#     x = int(input())
-#     total = total + x
-# print(total)
+total = 0
+for i in range(5):
+    x = int(input())
+    total = total + x
+print(total)
 
 
 # range(end)
